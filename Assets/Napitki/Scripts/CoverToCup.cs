90200312650b7bc73a3fac167f90825261744fcb
m1ndos <csgoezkatka@gmail.com>
2024-12-04 21:07:35 +0400

    Привязка крышки к стакану

--- Assets/Napitki/Scripts/CoverToCup.cs
+++ Assets/Napitki/Scripts/CoverToCup.cs
@@ -1,43 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CoverToCup : MonoBehaviour
 {
-    public GameObject cover;  // Объект крышки
-    public GameObject cup;    // Объект стакана
-    public Transform snapPoint;  // Точка привязки для крышки
-    public Quaternion desiredRotation;  // Желаемое вращение крышки
+    public GameObject cover;
 
-    private void OnTriggerEnter(Collider colider)
+    private void OnTriggerEnter(Collider other)
     {
-        // Проверяем, что столкновение с объектом с тегом "Cover"
-        if (!colider.gameObject.CompareTag("Cover")) return;
-
-        // Получаем позицию верхней части стакана
-        Vector3 cupTop = cup.transform.position + cup.transform.up * (cup.GetComponent<Renderer>().bounds.size.y / 2);
-
-        // Перемещаем крышку на верхнюю часть стакана, учитывая точное положение
-        colider.transform.position = cupTop;
-
-        // Настроим вращение крышки так, чтобы оно совпало с вращением стакана
-        colider.transform.rotation = cup.transform.rotation;
-
-        // Делаем крышку дочерним элементом стакана, чтобы они двигались вместе (если это необходимо)
-        //colider.transform.SetParent(cup.transform);
-        colider.transform.parent = snapPoint;
-
-        // Отключаем гравитацию у Rigidbody крышки
-        Rigidbody coverRigidbody = colider.GetComponent<Rigidbody>();
-        if (coverRigidbody != null)
-        {
-            coverRigidbody.useGravity = false;  // Отключаем гравитацию
-            coverRigidbody.isKinematic = true;  // Делаем объект кинематическим (не подчиняется физике)
+        if(other.CompareTag("Cover")){
+            Destroy(other.gameObject);
+            cover.SetActive(true);
         }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        // Логика для выхода, если нужно
     }
 }
